64bfc2fd83d9630581de7bc2af32842ebd82cb38
TexasBearKat <125840338+TexasBearKat@users.noreply.github.com>
2024-06-29 21:24:59 -0500

    Update both
    adding more stuff

--- function.py
+++ function.py
@@ -3,11 +3,22 @@ import random as r
 def roll():
   return r.randint(1,6)
 
+
 def getStat():
   list = (roll(), roll(), roll(), roll())
   return sum(list) - min(list)
 
+
 def getStatBlock():
   return (getStat(), getStat(), getStat(), getStat(), getStat(), getStat())
 
-print(getStatBlock())
+
+def ValueErrorCheck(var):
+  try:
+    var = int(input(f"{var}: "))
+  except ValueError:
+    print(f"Set to 3")
+    var = 3
+  return var
+
+  
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import function as f
 import random as r
+
+target = f.ValueErrorCheck("Target")
+count = f.ValueErrorCheck("Count")
+least = f.ValueErrorCheck("Least")
