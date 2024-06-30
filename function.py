d0b208f18f6f9d060d320efd7cddc401f592131e
TexasBearKat <125840338+TexasBearKat@users.noreply.github.com>
2024-06-29 21:13:53 -0500

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,14 @@
+import random as r
 
+def roll():
+  return r.randint(1,6)
+
+def getStat():
+  list = (roll(), roll(), roll(), roll())
+  return sum(list) - min(list)
+
+def getStatBlock():
+  return (getStat(), getStat(), getStat(), getStat(), getStat(), getStat())
+
+print(getStatBlock)
+  
